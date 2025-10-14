0a20ec09c32f86c400b21e7c7a32b9806dc08fee
Victor Adossi <vadossi@cosmonic.com>
2025-10-15 01:18:49 +0900

    fix: fix wasi sdk path on windows

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,9 +1,12 @@
 string(TOLOWER ${CMAKE_HOST_SYSTEM_NAME} HOST_OS)
 cmake_host_system_information(RESULT HOST_CPU QUERY OS_PLATFORM)
 
-
 if (${HOST_OS} STREQUAL "darwin")
     set(HOST_OS "macos")
+endif()
+
+if (${HOST_CPU} STREQUAL "AMD64")
+    set(HOST_CPU "x86_64")
 endif()
 
 include("compile-flags")
